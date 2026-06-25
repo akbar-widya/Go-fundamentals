0b37a20b6b38392a368602a80e11277d83cadb7d
akbar-widya <akbarwidya.dev@gmail.com>
2026-06-26 04:19:39 +0700

    3. Function III - naked returns

--- function/main.go
+++ function/main.go
@@ -2,13 +2,19 @@ package main
 
 import "fmt"
 
-func minMax(nums []int) (int, int) {
-    // Returning dummy data for this example
-	return nums[0], nums[1]
+func minMax(nums []int) (min int, max int) {
+    min = nums[0]
+	max = nums[0]
+
+	for _, n := range nums {	// n is the value from index 0, then the program is loop with the next index's value
+		if n < min { min = n }
+		if n > max { max = n }
+	}
+	return
 }
 
 func main() {
-	min, _ := minMax([]int{3, 1, 4, 1, 5})
+	min, max := minMax([]int{3, 1, 4, 1, 5})
 
-	fmt.Println("Min:", min)
+	fmt.Println("Min:", min, "Max:", max)
 }
